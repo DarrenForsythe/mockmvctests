f82552f23f5c73164a1a3e8d925317d796bed274
Darren Forsythe <git@darrenforsythe.com>
2018-07-16 15:35:01 +0100

    add second controller. Only load RootController with MockMvcTest

--- src/test/java/com/darrenforsythe/mockmvc/RootControllerMockMvcTest.java
+++ src/test/java/com/darrenforsythe/mockmvc/RootControllerMockMvcTest.java
@@ -16,7 +16,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest
+@WebMvcTest(controllers = RootController.class)
 public class RootControllerMockMvcTest {
 
     @Autowired
